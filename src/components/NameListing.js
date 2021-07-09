b591513a676fcd7ead82f6e34f17d097b133c01c
zubair-saif <zubairsaif700@gmail.com>
2021-07-09 18:49:54 +0500

    Lists and Keys & Index as Key Anti-pattern

--- src/components/NameListing.js
+++ src/components/NameListing.js
@@ -2,25 +2,28 @@ import React from "react";
 import Person from "./Person";
 
 function NameListing() {
-//   let arrayOFName = ["Juniad", "Khazir", "Ali"];
+  //   let arrayOFName = ["Juniad", "Khazir", "Ali"];
   let person = [
     {
+      id: 1,
       name: "junaid",
       age: "25",
       skill: "angular",
     },
     {
+      id: 2,
       name: "tabrak",
       age: "43",
       skill: "react",
     },
     {
+      id: 3,
       name: "ali",
       age: "43",
       skill: "Vue",
     },
   ];
-  let personList = person.map((o) => <Person person={o}/>);
+  let personList = person.map((o) => <Person key={o.id} person={o} />);
   return <div>{personList}</div>;
 }
 
