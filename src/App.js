5d5ea6b71f896c0eb2e0d2ef1814329ccd6ef226
zubair-saif <zubairsaif700@gmail.com>
2021-07-09 16:05:33 +0500

    pass data to children component using prop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import React, { Component } from 'react'
-
+import Greet from "./components/Greet";
 class App extends Component {
  
   render() {
-    return <div className="App"><h1>Welcome React</h1></div>;
+    return <div className="App">
+      <h1>Welcome React</h1>
+      <Greet name="Welcome"/>
+      <Greet name="juni"/>
+      <Greet name="juni"/>
+
+      </div>;
   }
 }
 
---
+++ src/components/Greet.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const Greet = (props) => {
+    return (
+        <div>
+            <h1>{props.name}</h1>
+        </div>
+    )
+};
+
+export default Greet;
