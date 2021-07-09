b2ab30286ee425299a6f5f5fd604f75dc8aa832e
zubair-saif <zubairsaif700@gmail.com>
2021-07-09 17:00:25 +0500

    Event Handling

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import React, { Component } from 'react'
 import Greet from "./components/Greet";
 import Message from "./components/Message";
 import Counter from "./components/Counter";
+import FunctionClick from "./components/FunctionClick";
 
 class App extends Component {
  
   render() {
     return <div className="App">
       {/* <h1>Welcome React</h1> */}
-      <Greet name="Welcome" heroName="hero"/>
+      {/* <Greet name="Welcome" heroName="hero"/> */}
       {/* <Message/> */}
       <Counter/>
+      <FunctionClick></FunctionClick>
       </div>;
   }
 }
